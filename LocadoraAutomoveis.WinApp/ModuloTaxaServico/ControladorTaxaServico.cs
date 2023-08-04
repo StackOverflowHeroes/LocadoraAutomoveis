2825f654b1c6feb64ab020844e2584b49d79f929
Viitor Montunes <79266568+Viito404@users.noreply.github.com>
2023-08-04 14:39:59 -0300

    feat: implementado TabelaTaxaServicoControl em Apresentacao

---
+++ LocadoraAutomoveis.WinApp/ModuloTaxaServico/ControladorTaxaServico.cs
@@ -0,0 +1,33 @@
+﻿using LocadoraAutomoveis.Aplicacao.ModuloTaxaServico;
+using LocadoraAutomoveis.Dominio.ModuloTaxaServico;
+
+namespace LocadoraAutomoveis.WinApp.ModuloTaxaServico
+{
+     public class ControladorTaxaServico : ControladorBase
+     {
+          private IRepositorioTaxaServico repositorioTaxaServico;
+          private ServicoTaxaServico servicoTaxaServico;
+          private TabelaTaxaServicoControl tabelaTaxaServico;
+
+          public override void Inserir()
+          {
+               throw new NotImplementedException();
+          }
+          public override void Editar()
+          {
+               throw new NotImplementedException();
+          }
+          public override void Excluir()
+          {
+               throw new NotImplementedException();
+          }
+          public override ConfiguracaoToolboxBase ObtemConfiguracaoToolbox()
+          {
+               return new ConfiguracaoToolboxTaxaServico();
+          }
+          public override UserControl ObtemListagem()
+          {
+               throw new NotImplementedException();
+          }
+     }
+}
---
+++ LocadoraAutomoveis.WinApp/ModuloTaxaServico/TabelaTaxaServicoControl.cs
@@ -0,0 +1,44 @@
+﻿using LocadoraAutomoveis.Dominio.ModuloTaxaServico;
+
+namespace LocadoraAutomoveis.WinApp.ModuloTaxaServico
+{
+     public partial class TabelaTaxaServicoControl : UserControl
+     {
+          public TabelaTaxaServicoControl()
+          {
+               InitializeComponent();
+               gridTaxaServico.ConfigurarGridZebrado();
+               gridTaxaServico.ConfigurarGridSomenteLeitura();
+               gridTaxaServico.Columns.AddRange(ObterColunas());
+          }
+
+          private DataGridViewColumn[] ObterColunas()
+          {
+               var colunas = new DataGridViewColumn[]
+{
+                new DataGridViewTextBoxColumn { Name = "Id", HeaderText = "Id", FillWeight=15F, Visible=false },
+
+                new DataGridViewTextBoxColumn { Name = "Nome", HeaderText = "Nome", FillWeight=85F },
+
+                new DataGridViewTextBoxColumn { Name = "Preco", HeaderText = "Preço da Taxa/Serviço", FillWeight=50F }
+};
+
+               return colunas;
+          }
+
+          public Guid ObtemIdSelecionada()
+          {
+               return gridTaxaServico.SelecionarId();
+          }
+
+          public void AtualizarRegistros(List<TaxaServico> taxaServicos)
+          {
+               gridTaxaServico.Rows.Clear();
+
+               foreach (TaxaServico taxaServico in taxaServicos)
+               {
+                    gridTaxaServico.Rows.Add(taxaServico.Id, taxaServico.Nome);
+               }
+          }
+     }
+}
