d8b81fe4cb61727484fc18ea9eecd01d97789314
jotaskalski <juan.skalski96@hotmail.com>
2023-08-07 17:43:21 -0300

    add: implementado modulo aluguel na camada domínio

---
+++ LocadoraAutomoveis/ModuloAluguel/IRepositorioAluguel.cs
@@ -0,0 +1,10 @@
+﻿
+using LocadoraAutomoveis.Dominio.ModuloCupom;
+
+namespace LocadoraAutomoveis.Dominio.ModuloAluguel
+{
+    public interface IRepositorioAluguel : IRepositorio<Aluguel>
+    {
+        bool CumpomExiste(Aluguel aluguelParaValidar, List<Cupom> cupomLista);
+    }
+}
