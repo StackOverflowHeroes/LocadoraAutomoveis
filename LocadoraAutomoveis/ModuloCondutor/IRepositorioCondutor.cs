63a9e84a1c22ee9dc87e43854f48d2f325d0badd
Viitor Montunes <79266568+Viito404@users.noreply.github.com>
2023-08-08 19:22:29 -0300

    feat: implementado entidade Condutor e IRepositorioCondutor

---
+++ LocadoraAutomoveis/ModuloCondutor/IRepositorioCondutor.cs
@@ -0,0 +1,9 @@
+﻿namespace LocadoraAutomoveis.Dominio.ModuloCondutor
+{
+     public interface IRepositorioCondutor : IRepositorio<Condutor>
+     {
+          Condutor SelecionarPorNome(string nome);
+
+          public List<Condutor> SelecionarTodos(bool incluirCliente = false);
+     }
+}
