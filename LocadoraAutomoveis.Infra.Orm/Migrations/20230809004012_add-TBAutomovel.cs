ca8f39488f2840058aabbb05a82191fa160f9418
Leonardo <contato.leoo0@gmail.com>
2023-08-08 21:46:36 -0300

    add: Mapeador e repositório ModuloAutomovel. Migration feita.

---
+++ LocadoraAutomoveis.Infra.Orm/Migrations/20230809004012_add-TBAutomovel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocadoraAutomoveis.Infra.Orm.Migrations
+{
+    /// <inheritdoc />
+    public partial class addTBAutomovel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBAutomovel",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ImagemAutomovel = table.Column<byte[]>(type: "image", nullable: false),
+                    GrupoAutomovelId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Modelo = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Marca = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Ano = table.Column<int>(type: "int", nullable: false),
+                    Quilometragem = table.Column<int>(type: "int", nullable: false),
+                    Cor = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Placa = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Combustivel = table.Column<int>(type: "int", nullable: false),
+                    CapacidadeLitros = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBAutomovel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TBAutomovel_TBGrupoAutomovel",
+                        column: x => x.GrupoAutomovelId,
+                        principalTable: "TBGrupoAutomovel",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBAutomovel_GrupoAutomovelId",
+                table: "TBAutomovel",
+                column: "GrupoAutomovelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBAutomovel");
+        }
+    }
+}
---
+++ LocadoraAutomoveis.Infra.Orm/ModuloAutomovel/RepositorioAutomovelEmOrm.cs
@@ -0,0 +1,26 @@
+﻿
+using LocadoraAutomoveis.Dominio.ModuloAutomovel;
+
+namespace LocadoraAutomoveis.Infra.Orm.ModuloAutomovel
+{
+    public class RepositorioAutomovelEmOrm : RepositorioBaseEmOrm<Automovel>, IRepositorioAutomovel
+    {
+        public RepositorioAutomovelEmOrm(GeradorTestesDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public Automovel SelecionarPorNome(string nome)
+        {
+            return registros.FirstOrDefault(x => x.Modelo == nome);
+
+        }
+
+        public List<Automovel> SelecionarTodos(bool incluirGrupoAutomovel = false)
+        {
+            if (incluirGrupoAutomovel)
+                return registros.Include(x => x.GrupoAutomovel).ToList();
+
+            return registros.ToList();
+        }
+    }
+}
